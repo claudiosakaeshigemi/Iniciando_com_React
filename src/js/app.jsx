101d0497539649099c70d37df381f0dcd6ed8187
Dimsol <claudiosakaeshigemi@gmail.com>
2018-08-24 21:21:04 -0300

    aula 3

---
+++ src/js/app.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+let App = (
+  <div className="container">
+    <h1> Olá mundo !</h1>
+  </div>
+);
+
+ReactDOM.render(App, document.getElementById('id'))
